637c890551bd8afd1c2ee5e535fb4b8cba5271da
Daigo Matsuoka <marcelo.daigo@gmail.com>
2017-08-19 01:15:36 -0300

    Creates the manager layer to communicate with view models

---
+++ app/src/main/java/io/wwdaigo/topmovies/remote/manager/MoviesManager.java
@@ -0,0 +1,27 @@
+package io.wwdaigo.topmovies.remote.manager;
+
+import javax.inject.Inject;
+
+import io.reactivex.Observable;
+import io.wwdaigo.topmovies.data.BaseResponse;
+import io.wwdaigo.topmovies.data.MovieData;
+import io.wwdaigo.topmovies.remote.request.MoviesRequest;
+import retrofit2.adapter.rxjava2.Result;
+
+/**
+ * Created by daigomatsuoka on 19/08/17.
+ */
+
+public final class MoviesManager {
+
+    @Inject
+    MoviesRequest moviesRequest;
+
+    public Observable<Result<BaseResponse<MovieData>>> getPopular() {
+        return moviesRequest.getMoviePopular();
+    }
+
+    public Observable<Result<BaseResponse<MovieData>>> getTopRated() {
+        return moviesRequest.getMovieTopRated();
+    }
+}
