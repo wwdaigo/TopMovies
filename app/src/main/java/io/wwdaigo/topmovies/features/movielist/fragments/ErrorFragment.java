0f010ddec752c2b5609d24bbf27feb3857bdc5f8
Daigo Matsuoka <marcelo.daigo@gmail.com>
2017-08-20 13:46:47 -0300

    Implements error fragment class

--- app/src/main/java/io/wwdaigo/topmovies/features/movielist/fragments/ErrorFragment.java
+++ app/src/main/java/io/wwdaigo/topmovies/features/movielist/fragments/ErrorFragment.java
@@ -1,6 +1,7 @@
 package io.wwdaigo.topmovies.features.movielist.fragments;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -8,81 +9,49 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jakewharton.rxbinding.view.RxView;
+
 import io.wwdaigo.topmovies.R;
+import io.wwdaigo.topmovies.databinding.FragmentErrorBinding;
+import rx.functions.Action1;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ErrorFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ErrorFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ErrorFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    private OnErrorFragmentInteraction mListener;
+    private FragmentErrorBinding binding;
 
     public ErrorFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ErrorFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ErrorFragment newInstance(String param1, String param2) {
-        ErrorFragment fragment = new ErrorFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static ErrorFragment newInstance() {
+        return new ErrorFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_error, container, false);
-    }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        binding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_error, container, false);
+
+        bindEvents();
+        return binding.getRoot();
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof OnErrorFragmentInteraction) {
+            mListener = (OnErrorFragmentInteraction) context;
         } else {
             throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+                    + " must implement OnErrorFragmentInteraction");
         }
     }
 
@@ -92,18 +61,14 @@ public class ErrorFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    private void bindEvents() {
+        RxView.clicks(binding.retryButton).subscribe(new Action1<Void>() {
+            @Override
+            public void call(Void aVoid) {
+                if (mListener != null) {
+                    mListener.retryAction();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/io/wwdaigo/topmovies/features/movielist/fragments/OnErrorFragmentInteraction.java
@@ -0,0 +1,9 @@
+package io.wwdaigo.topmovies.features.movielist.fragments;
+
+/**
+ * Created by daigomatsuoka on 20/08/17.
+ */
+
+public interface OnErrorFragmentInteraction {
+    void retryAction();
+}
