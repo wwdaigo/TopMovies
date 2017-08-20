a58f1d2e86bb732d2be8004dee0a684ca57bbfbd
Daigo Matsuoka <marcelo.daigo@gmail.com>
2017-08-20 00:52:22 -0300

    Creates the Detail Activity

---
+++ app/src/main/java/io/wwdaigo/topmovies/features/details/DetailsActivity.java
@@ -0,0 +1,15 @@
+package io.wwdaigo.topmovies.features.details;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import io.wwdaigo.topmovies.R;
+
+public class DetailsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
