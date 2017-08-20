5f263a6293564d2da60c82ce317d18aa9c600a5e
Daigo Matsuoka <marcelo.daigo@gmail.com>
2017-08-20 01:02:50 -0300

    Creates the routing linking Details Activity

--- app/src/main/java/io/wwdaigo/topmovies/features/details/DetailsActivity.java
+++ app/src/main/java/io/wwdaigo/topmovies/features/details/DetailsActivity.java
@@ -1,11 +1,25 @@
 package io.wwdaigo.topmovies.features.details;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 import io.wwdaigo.topmovies.R;
+import io.wwdaigo.topmovies.data.MovieData;
 
 public class DetailsActivity extends Activity {
+
+    public static Intent startIntent(Context context, MovieData movieData) {
+
+        final String EXTRA_MOVIE_DATA = "extraMovieData";
+
+        Intent intent = new Intent(context, DetailsActivity.class);
+        intent.putExtra(EXTRA_MOVIE_DATA, movieData);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/io/wwdaigo/topmovies/router/MainRouter.java
+++ app/src/main/java/io/wwdaigo/topmovies/router/MainRouter.java
@@ -3,6 +3,7 @@ package io.wwdaigo.topmovies.router;
 import android.content.Context;
 
 import io.wwdaigo.topmovies.data.MovieData;
+import io.wwdaigo.topmovies.features.details.DetailsActivity;
 
 /**
  * Created by daigomatsuoka on 20/08/17.
@@ -12,6 +13,6 @@ public final class MainRouter implements MainRouterType {
 
     @Override
     public void openMovie(Context context, MovieData movieData) {
-
+        context.startActivity(DetailsActivity.startIntent(context, movieData));
     }
 }
