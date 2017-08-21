d3e8dfff5a39efb8c767a709fe3a61272a0d080e
Daigo Matsuoka <marcelo.daigo@gmail.com>
2017-08-20 23:33:58 -0300

    Creates the Search Activity

---
+++ app/src/main/java/io/wwdaigo/topmovies/features/search/SearchActivity.java
@@ -0,0 +1,15 @@
+package io.wwdaigo.topmovies.features.search;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.wwdaigo.topmovies.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
